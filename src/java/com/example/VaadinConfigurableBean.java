e1627acde7397e9394404b299b2c35a7a2888318
Archie L. Cobbs <archie@dellroad.org>
2012-10-19 17:13:16 -0500

    Show use of ifSessionNotLocked.

--- src/java/com/example/VaadinConfigurableBean.java
+++ src/java/com/example/VaadinConfigurableBean.java
@@ -4,13 +4,14 @@ package com.example;
 import com.vaadin.server.SessionDestroyEvent;
 import com.vaadin.server.SessionDestroyListener;
 
+import org.dellroad.stuff.java.ErrorAction;
 import org.dellroad.stuff.vaadin7.VaadinConfigurable;
 
 /**
  * Example of a Spring bean that lives in the per-Applicaton application context
  * and is autowired by the @{@link VaadinConfigurable} aspect.
  */
-@VaadinConfigurable
+@VaadinConfigurable(ifSessionNotLocked = ErrorAction.EXCEPTION)
 public class VaadinConfigurableBean extends AbstractApplicationBean implements SessionDestroyListener {
 
     @Override
